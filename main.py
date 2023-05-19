9ae84b35fb85c47bf5838937fc1efb7e7b7c7e4c
Diana Cardona <dscardona@gmail.com>
2023-05-19 15:12:51 -0400

    Restructure to fix bugs

--- main.py
+++ main.py
@@ -1,32 +1,16 @@
 from data import MENU, resources 
-money_received = 0
-change_rendered = 0
-total_in_machine = money_received - change_rendered
+profit = 0
 
-def enough_resources(chosen_drink):
-    """Receives the drink selected by the user, determines whether there are enough resources to make the drink, returns boolean."""
-    recipe = MENU[chosen_drink]["ingredients"]
+def enough_resources(chosen_drink_ingredients):
+    """Receives the ingredients of the drink selected by the user, determines whether there are enough resources to make the drink, returns boolean."""
+    for item in recipe:
+        if chosen_drink_ingredients[item] > resources[item]:
+            print(f"Sorry, there is not enough {item}")
+            return False
+        return True
 
-    needed_water = recipe["water"]
-    needed_coffee = recipe["coffee"]
-    needed_milk = 0
-    if "milk" in recipe:
-        needed_milk = recipe["milk"]
-
-    if needed_water > resources["water"]:
-        print("Sorry, there's not enough water")
-        return False
-    if needed_coffee > resources["coffee"]:
-        print("Sorry, there's not enough coffee")
-        return False
-    if needed_milk > resources["milk"]:
-        print("Sorry, there isn'e enough milk")
-        return False
-
-    return True
-
-def count_coins():
-    """Gets number/kind of coins entered, returns amount entered"""
+def process_coins():
+    """Returns the total calculated fron coins inserted."""
 
     print("Please insert coins,")
     quarters_amt = int(input("How many quarters? ")) * .25
@@ -39,61 +23,48 @@ def count_coins():
 
 def print_report():
     print(f"\nAmount of water: {resources['water']}")
-    print(f"\nAmount of coffee: {resources['coffee']}")
-    print(f"\nAmount of milk: {resources['milk']}\n")
+    print(f"Amount of coffee: {resources['coffee']}")
+    print(f"Amount of milk: {resources['milk']}\n")
 
-    print(f"Amont of money in register: {total_in_machine}\n")
+    print(f"Amount of money in register: {profit}\n")
+
+def successful_transaction(money_received, drink_cost):
+    # """Returns True if amount sufficient and False if it isnt't."""
+    if money_received >= cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is ${change} in change.")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry, that isn't enoug. Money refunded. \n")
+        return False
+
+def make_coffee(drink_name, order_ingredients):
+    """Deduct required ingredients from resources."""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name}. Enjoy! ☕")
 
 
 coffee_machine_on = True
 
-def coffee_machine_on():
+while coffee_machine_on:
     choice = input("Please enter a number: \n1. Espresso \n2. Latte \n3. Cappuccino\n" )
 
-    drink_to_make = ""
-    if choice == "1":
-        drink_to_make = "espresso"
-    elif choice == "2":
-        drink_to_make = "latte"
-    elif choice == "3":
-        drink_to_make = "cappuccino"
+    if choice == "off":
+        coffee_machine_on = False
     elif choice == "report":
         print_report()
-        coffee_machine_on()
-    elif choice == "off":
-        return
     else:
-        print("That's not an option")
-        coffee_machine_on()
-
-    if choice == "1" or choice == "2" or choice =="3":
-        total_entered = 0
-        if enough_resources(drink_to_make):
-            total_entered = count_coins()
-
-        cost = MENU[drink_to_make]["cost"] 
-        if cost == total_entered:
-            money_received += total_entered
-            print(f"Here's your {drink_to_make}. Enjoy!")
-            resources["water"] -= MENU[drink_to_make]["ingredients"]["water"]
-            resources["coffee"] -= MENU[drink_to_make]["ingredients"]["coffee"]
-            if "milk" in MENU[drink_to_make]["ingredients"]:
-                resources["milk"] -= MENU[drink_to_make]["ingredients"]["milk"]
-
-        elif cost < total_entered:
-            money_received += cost
-            change = abs(cost - total_entered)
-            print(f"Here's your change: ${change}")
-            change_rendered += change
-            print(f"Here's your {drink_to_make}. Enjoy!")
-            resources["water"] -= MENU[drink_to_make]["ingredients"]["water"]
-            resources["coffee"] -= MENU[drink_to_make]["ingredients"]["coffee"]
-            if "milk" in MENU[drink_to_make]["ingredients"]:
-                resources["milk"] -= MENU[drink_to_make]["ingredients"]["milk"]
-
-        else:
-            print(f"Sorry, that's not enough. Here's your refund: ${total_entered}")
-
-coffee_machine_on()
-
-#TODO: If a resource runs out, only the message stating so should be displayed. The count_coins function shouldn't be executed
+        if choice == "1":
+            choice = "espresso"
+        elif choice == "2":
+            choice = "latte"
+        elif choice == "3":
+            choice = "cappuccino"
+        drink = MENU[choice]
+        if enough_resources(drink["ingredients"]):
+            payment = process_coins()
+            if successful_transaction(payment, drink["cost"]):
+                make_coffee(choice, drink["ingredients"])
