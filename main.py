451ab6c4ffad56f956d602634d9dce1f0b05a7a9
Diana Cardona <dscardona@gmail.com>
2023-05-18 21:10:42 -0400

    Create to do list

---
+++ main.py
@@ -0,0 +1,43 @@
+
+
+# TODO: Define variables: resources, money counter
+# TODO: Function to determine if there are enough resources, Function to determine if transaction was successful, Function to return change, Function to "Make coffee": deduct resources, add money to money counter and print success.
+
+# TODO: Prompt user to make a choice
+        # TODO:Check if there's enough Water, Milk, and Coffee to make user's selected drink. Let user know if resources arent sufficient "Sorry, there's not enough water"
+        # Function to determine if there are enough resources, returns boolean. returns true or false.
+
+        #TODO: If there are enough resources, prompt user to insert coins. Calculate value of the coins inserted.
+            # TODO: Check if user didn't insert enough coins: “Sorry that's not enough money. Money refunded.” Prompt again. return 0.
+ 
+            #TODO: If enough coins, returns amount inserted.
+
+            #TODO: Function to return negative value if too much money was inserted. 
+        
+
+        #TODO: If transaction successful (zero not returned from coins inserted check function), and there are enough resources (resources function returns True) , deduct resources from the machine and tell the user: “Here is your {drink_of_choice}. Enjoy!” Print change being returned.
+
+    # TODO: Keep prompting user for choice, while loop with off flag or recursion (new "coffee-machine-on "function)
+
+# TODO: End execution with word "off" 
+
+# TODO: Print report with current Water, Milk, Coffee and Money values, with word "report". Prompt again.
+
+
+# RECIPES:
+#     Espresso:
+#     1.50
+#     50ml water
+#     18g coffee
+
+#     Latte:
+#     2.50
+#     200ml water
+#     24g coffee
+#     150ml Milk
+
+#     Cappuccino:
+#     3.00
+#     250ml water
+#     24g coffee
+#     100ml milk
